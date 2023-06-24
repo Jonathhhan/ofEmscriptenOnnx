5e82ffc522eced83dde4acc35cfd1e22e27e7a31
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2023-06-24 06:07:37 +0200

    Add files via upload

---
+++ quick-start_onnxruntime-web-bundler/main.js
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+const ort = require('onnxruntime-web');
+var onnxSession;
+
+startOnnxSession = function() {
+	// create a new session and load the specific model.
+	ort.InferenceSession.create("onnx/model.onnx").then((session) => {
+		onnxSession = session;
+		Module.onnx("status", "dummy");
+	});
+}
+
+onnxInference = function(destinationTextureID1, destinationTextureID2) {
+        // the model in this example contains a single MatMul node
+        // it has 2 inputs: 'a'(float32, 3x4) and 'b'(float32, 4x3)
+        // it has 1 output: 'c'(float32, 3x3)
+
+	// prepare inputs. a tensor need its corresponding TypedArray as data
+	const dataA = Float32Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+	const dataB = Float32Array.from([10, 20, 30, 40, 50, Math.floor(Math.random() * 60), 70, 80, Math.floor(Math.random() * 90), 100, 110, Math.floor(Math.random() * 120)]);
+	const tensorA = new ort.Tensor("float32", dataA, [3, 4]);
+	const tensorB = new ort.Tensor("float32", dataB, [4, 3]);
+
+	// prepare feeds. use model input names as keys.
+	const feeds = { a: tensorA, b: tensorB };
+
+	// feed inputs and run
+	onnxSession.run(feeds).then((results) => {
+
+		// read from results
+		const dataC = results.c.data;
+		console.log(`Data of result tensor 'c': ${dataC}`);
+		Module.onnx("inference", `Data of result tensor 'c': ${dataC}`);
+	});
+}
---
+++ src/ofApp.cpp
@@ -0,0 +1,95 @@
+#include "ofApp.h"
+
+std::string onnxModelStatus;
+std::string inferenceResult;
+bool onnxModelIsLoaded;
+
+void onnx(std::string info, std::string inference) {
+	if (info == "status") {  
+		onnxModelStatus = "Onnx model loaded.";
+		onnxModelIsLoaded = true;
+	} else if (info == "inference") {
+		inferenceResult = inference;
+	}
+}
+
+//--------------------------------------------------------------
+EMSCRIPTEN_BINDINGS(Module){
+	emscripten::function("onnx", &onnx);
+}
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	onnxModelIsLoaded = false;
+	ofSetBackgroundColor(100, 100, 100);
+	onnxModelStatus = "Onnx model loading.";
+	EM_ASM(
+		startOnnxSession();
+	);
+}
+ 
+//--------------------------------------------------------------
+void ofApp::update(){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	ofDrawBitmapString(onnxModelStatus, 20, 20);
+	ofDrawBitmapString(inferenceResult, 20, 40);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+	if (onnxModelIsLoaded) {
+		EM_ASM(onnxInference());
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+
+}
